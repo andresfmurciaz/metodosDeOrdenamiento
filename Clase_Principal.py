955921fee9e9a3d8c8742197d9801b456ce184a2
andresf.murciaz <andresf.murciaz@gmail.com>
2023-03-19 17:00:04 -0500

    arreglando main

--- Clase_Principal.py
+++ Clase_Principal.py
@@ -6,7 +6,14 @@ with open('Numeros.txt', 'r') as archivo:
     # Leer el contenido del archivo
     contenido = archivo.read()
     # Separar las líneas en elementos individuales y separar por comas
-    lista_datos = [linea.strip().split(',') for linea in contenido.split('\n')]
-
+    lista_datos = [linea.strip() for linea in contenido.split(',')]
 print(lista_datos)
+
+#--------------------------imprime y pregunta
+if __name__== '__main__':
+ fin_lista = int(input('¿Cuantos numeros quiere organizar?'))
+listaEstimacion = lista_datos[0:fin_lista]
+print(listaEstimacion)
+
+
 
--- seg.py
+++ seg.py
@@ -2,27 +2,30 @@ import random
 import time
 inicio = time.time()
 #metodo burbuja 
+class hola:
+    def __init__(self,lista):
+        self.lista=lista
+        self.burbuja =self.ordenamiento_de_burbuja(lista)
 
+    def ordenamiento_de_burbuja(self,lista):
+        n = len(lista)
 
-
-def ordenamiento_de_burbuja(lista):
-    n = len(lista)
-
-    for i in range(n):                                              
-     for j in range(0, n - i - 1):
-        if lista[j] > lista[j + 1]:
-            lista[j], lista[j + 1] = lista[j + 1], lista[j]
-    return lista
+        for i in range(n):
+         for j in range(0, n - i - 1):
+            if lista[j] > lista[j + 1]:
+                lista[j], lista[j + 1] = lista[j + 1], lista[j]
+        return lista
 
 
 if __name__== '__main__':
- tamano_de_lista = int(input('De que tamano sera la lista? ')) 
 
- lista = [random.randint(0,1000 ) for i in range(tamano_de_lista)]
- print(lista)
 
- lista_ordenada = ordenamiento_de_burbuja(lista)
- print(lista_ordenada)
+ tamano_de_lista = int(input('De que tamano sera la lista?'))
 
+ lista2 = [random.randint(0,1000 ) for i in range(tamano_de_lista)]
+ print(lista2)
+ hola1= hola(lista2)
+ listaordenada = hola1.ordenamiento_de_burbuja(lista2)
+ print(listaordenada)
 fin = time.time()
 print(fin-inicio),input('tiempo de ejecucion del algoritmo')
